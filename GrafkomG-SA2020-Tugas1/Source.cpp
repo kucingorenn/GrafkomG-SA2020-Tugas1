b41ec7e5fc5f93fc4a8aabe3f241158b0886253a
kucingorenn <65481065+kucingorenn@users.noreply.github.com>
2020-05-18 18:04:15 +0700

    Tambah Kotak, pintu, jendela

--- GrafkomG-SA2020-Tugas1/Source.cpp
+++ GrafkomG-SA2020-Tugas1/Source.cpp
@@ -5,11 +5,56 @@ void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1.0, 0.0, 0.0);
 
+	//ATAP
 	glBegin(GL_TRIANGLES);
-	glVertex2f(0.0, 0.0);
-	glVertex2f(499.0, 0.0);
-	glVertex2f(250.0, 499.0);
+		glVertex2f(400, 400);
+		glVertex2f(350, 300);
+		glVertex2f(450, 300);
 	glEnd();
+
+	glBegin(GL_QUADS);
+		glVertex2f(360, 300);
+		glVertex2f(360,	200);
+		glVertex2f(440,	200);
+		glVertex2f(440,	300);
+	glEnd();
+
+	glColor3f(1, 1, 1);
+	glBegin(GL_QUADS);
+		glVertex2f(370, 240);
+		glVertex2f(370, 205);
+		glVertex2f(390, 205);
+		glVertex2f(390, 240);
+	glEnd();
+
+	glColor3f(1, 1, 1);
+	glBegin(GL_QUADS);
+		glVertex2f(410, 250);
+		glVertex2f(410, 230);
+		glVertex2f(430, 230);
+		glVertex2f(430, 250);
+	glEnd();
+
+	glColor3f(1, 0, 0);
+	glBegin(GL_LINES);
+		glVertex2f(410, 240);
+		glVertex2f(430, 240);
+	glEnd();
+		
+	glColor3f(1, 0, 0);
+	glBegin(GL_LINES);
+		glVertex2f(420, 250);
+		glVertex2f(420, 230);
+	glEnd();
+
+	glColor3f(0, 0, 0);
+	glBegin(GL_LINE_LOOP);
+		glVertex2f(410, 250);
+		glVertex2f(410, 230);
+		glVertex2f(430, 230);
+		glVertex2f(430, 250);
+	glEnd();
+
 	glFlush();
 }
 
@@ -19,7 +64,7 @@ void myinit() {
 	glPointSize(2.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(0.0, 499.0, 0.0, 499.0);
+	gluOrtho2D(0.0, 500.0, 0.0, 500.0);
 }
 
 int main(int argc, char** argv) {
@@ -27,7 +72,7 @@ int main(int argc, char** argv) {
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(0, 0);
-	glutCreateWindow("Points");
+	glutCreateWindow("Tugas1 - 672018274");
 	glutDisplayFunc(display);
 
 	myinit();
